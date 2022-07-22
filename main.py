aae9906ba50343d43fe6721484659754a03a7863
Daniel Gallagher <dangallagher@Daniels-MacBook-Pro.local>
2022-07-22 10:44:51 +0100

    Add model evaluation

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pathlib
 
 if __name__ == '__main__':
     
+    # DEV
+    # -------------
     #change_metadata()
     #create_img_filepaths_array()
     
@@ -16,24 +18,23 @@ if __name__ == '__main__':
     #res = preprocess_image(images)
     
     #create_imagedata_dir()
-    
-    # TRAINING LOOP
     # -------------
     
     
+    # TRAINING LOOP
+    # -------------
+    # Load datasets
     train_gen, val_gen, test_gen = create_dataset_generators()
     
-    EPOCHS = 3
-    BATCH_SIZE = 32
-    
-    
+    # Load model
     model = create_baseline()
     
-    
-    model.fit_generator(generator=train_gen,
+    # Fit training data
+    history = model.fit(x=train_gen,
                         validation_data=val_gen,
                         steps_per_epoch=train_gen.n//train_gen.batch_size,
                         validation_steps=val_gen.n//val_gen.batch_size,
-                        epochs=3)
+                        epochs=15)
     
-    
+    # Evaluate 
+    model.evaluate(x=test_gen)
